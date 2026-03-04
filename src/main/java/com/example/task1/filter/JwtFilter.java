ce798633e4b0b0beb31aa724abdaf7d08aac4022
narminmajidova <mecidovne@gmail.com>
2026-03-05 00:00:42 +0400

    Commit

--- src/main/java/com/example/task1/filter/JwtFilter.java
+++ src/main/java/com/example/task1/filter/JwtFilter.java
@@ -77,5 +77,7 @@ public class JwtFilter extends OncePerRequestFilter {
                 return;
             }
         }
-        }
+        filterChain.doFilter(request, response);
+
+    }
     }
